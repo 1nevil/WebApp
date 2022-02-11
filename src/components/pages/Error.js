bd479f394cceecaec0e9fd65936b2444b5d5c671
nevil mistry <nevilmistry26@gmail.com>
2022-02-11 12:16:39 +0530

    added home page

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -4,7 +4,7 @@ export default function Error() {
   return (
     <div className="text-center mt-5 row">
       <div>
-        <img src="./images/sad.png" alt="Sad image" srcset="" />
+        <img src="./images/sad.png" />
         <h1 className="mt-3">Error 404 :Page is note found </h1>
         <p>The requested url is not found on this server.</p>
         <p>That's all we know</p>
